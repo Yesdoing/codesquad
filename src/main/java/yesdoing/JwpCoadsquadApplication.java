df24bb553b2aee8f5c403c5e2d90ad9b13d4c958
yesdoing <fkrmtleo12@gmail.com>
2017-07-31 18:55:15 +0900

    initial commit

---
+++ src/main/java/yesdoing/JwpCoadsquadApplication.java
@@ -0,0 +1,12 @@
+package yesdoing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwpCoadsquadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwpCoadsquadApplication.class, args);
+	}
+}
