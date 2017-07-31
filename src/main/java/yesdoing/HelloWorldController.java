3264414eabcf75ade68277a525a3668ab516dc6a
yesdoing <fkrmtleo12@gmail.com>
2017-07-31 19:16:49 +0900

    HelloWorldController 추가

---
+++ src/main/java/yesdoing/HelloWorldController.java
@@ -0,0 +1,13 @@
+package yesdoing;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+	
+	@GetMapping("/helloworld")
+	public String helloworld() {
+		return "Hello World!";
+	}
+}
