4770ff72727fac83a13666c2d2c26a55bcea169a
Jane Lee <yesdoing13@gmail.com>
2017-08-07 16:46:33 +0900

    로그인 로직 구현

--- src/main/java/yesdoing/dao/UserRepository.java
+++ src/main/java/yesdoing/dao/UserRepository.java
@@ -6,4 +6,5 @@ import org.springframework.data.repository.CrudRepository;
 import yesdoing.domain.User;
 
 public interface UserRepository extends CrudRepository<User, Long>{
+	User findByUserId(String userId);
 }
--- src/main/java/yesdoing/web/UserController.java
+++ src/main/java/yesdoing/web/UserController.java
@@ -1,12 +1,13 @@
 package yesdoing.web;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import yesdoing.dao.UserRepository;
@@ -61,5 +62,19 @@ public class UserController {
 	@GetMapping("/login")
 	public String loginForm() {
 		return "/user/login";
+	}
+	
+	@PostMapping("/login")
+	public String login(String userId, String password, HttpSession session) {
+		User user = userRepository.findByUserId(userId);
+		
+		if(user != null) {
+			if(user.getPassword().equals(password)) {
+				session.setAttribute("sessionedUser", user);
+				return "/";
+			} else {
+				return "redirect:/users/login";
+			}
+		} else return "redirect:/users/login";		
 	}
 }
